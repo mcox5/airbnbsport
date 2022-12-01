f33fc3bf359fa42996556713d7ca15921c5859d5
agustinjalil <agustinjalilleon@gmail.com>
2022-12-01 15:53:20 -0300

    products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_sign_up_params, only: [:create]
-end
 
-private
+  private
 
-def configure_sign_up_params
-  devise_parameter_sanitizer.permit(:sign_up, keys: %i[location])
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[location])
+  end
 end
