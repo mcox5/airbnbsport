fac66ec72a2f9222e8233005acb29857c3dab9be
Matias Cox <mcox5@uc.cl>
2022-12-02 11:10:23 -0300

    Modifique autorizaciones en controladore

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+
   def index
     @bookings = policy_scope(Booking)
   end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
 
   def index
     @products = policy_scope(Product)
